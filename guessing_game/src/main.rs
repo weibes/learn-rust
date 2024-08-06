fd02e65ed4c7b56ae5424f23f3b8d4be130e7c5f
Brendan Weibel <brweibel@microsoft.com>
2024-08-05 19:28:40 -0700

    guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,22 @@
 use rand::Rng;
 use std::io;
 use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, please guess a number between 1-100: ");
-    let mut guess = String::new();
     let random_number = rand::thread_rng().gen_range(1..=100);
     let mut num_guesses = 0;
     loop {
+        print!("Enter your guess: ");
+        let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("wow you suck");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please enter a number dumi");
+                continue;
+            }
+        };
         num_guesses += 1;
         match guess.cmp(&random_number) {
             Ordering::Less => println!("number is higher, try again."),
